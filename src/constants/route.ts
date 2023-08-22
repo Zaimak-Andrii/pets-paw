31e6891592fff8a8ea36dca76358b1e84d4bfa7f
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-08-22 15:53:57 +0300

    feat: add site navigation component

---
+++ src/constants/route.ts
@@ -0,0 +1,6 @@
+export enum Route {
+  HOME = '/',
+  VOTING = '/voting',
+  BREEDS = '/breeds',
+  GALLERY = '/gallery',
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,11 +11,29 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      transitionProperty: {
+        border: 'border-color',
+      },
+    },
+    screens: {
+      sm: '375px',
+      md: '768px',
+      xl: '1440px',
+    },
+    colors: {
+      light: '#f8f8f8',
+      dark: '#1d1d1d',
+      primary: '#8C8C8C',
+      white: '#FFFFFF',
+      purple: '#B4B7FF',
+      amber: '#FFD280',
+      'light-green': '#97EAB9',
+      'light-red': '#FF868E',
+      rose: '#FBE0DC',
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
